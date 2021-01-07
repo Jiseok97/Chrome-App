591882f73d0088f54fb01ba7a1ee88413fadf9a3
netLee <wltjr1426@naver.com>
2021-01-07 19:13:11 +0900

    Finished for ToDoList

--- JS/todo.js
+++ JS/todo.js
@@ -4,8 +4,33 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+// filter은 forEach에서 function을 실행하는 것 같이 각각의 item과 같이 실행될 예정
+function filterFn(toDo) {
+  return toDo.id === 1;
+}
+
 // List를 array로 만들기(항목이 많아질 수 있음)
-const toDos = [];
+let toDos = [];
+
+// toDo 삭제를 위한 함수
+function deleteToDo(event) {
+  // 아래 만든 버튼인 delBtn에 eventListener을 추가하여 삭제할 예정
+  // 작성 시 버튼의 조상을 찾아야 함! (parentNode 임) -> 그래야 어떤 li의 delBtn이 눌렸는지 모색할 수 있음!
+  const btn = event.target;
+  // 지워야 할 li인 btn.parentNode를 만들고,
+  const li = btn.parentNode;
+  // google에 검색시 childe element를 삭제하려면 removeChild를 사용하면 된다고 나와 있음
+  toDoList.removeChild(li);
+  // localStorage에 있는 li를 삭제해야 함! (새로고침시 삭제한 li가 다시 나오기 때문에)
+  // cleanToDos와 filter이 하는것은 filterFn이 체크가 된 아이템들의 array를 주는 것!
+  const cleanToDos = toDos.filter(function (toDo) {
+    // 여기서 toDo의 id는 숫자, li의 id는 문자열이라 바꿔줘야함! -> 그래서 li.id를 숫자로 바꿔줌
+    return toDo.id !== parseInt(li.id);
+  });
+  // 삭제한 array를 기존 toDo array에 대입시켜서 그것을 저장함으로써 완전 삭제 적용!!
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 // localStorage에 저장하기
 function saveToDos() {
@@ -20,6 +45,8 @@ function paintToDo(text) {
   // HTML에 Button 만들기, 그 버튼 모양은 아래의 X
   const delBtn = document.createElement("button");
   delBtn.innerText = "❌";
+  // 버튼을 누를시 삭제(위에 함수 호출)
+  delBtn.addEventListener("click", deleteToDo);
   // HTML에 span 만들기(가져온 text)
   const span = document.createElement("span");
   span.innerText = text;
